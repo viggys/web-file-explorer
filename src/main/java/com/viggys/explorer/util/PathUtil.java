dce6824476c3868c1a74cec6ee8b83735d433e98
Vignesh Subramanian <vignesh.subramanian93@gmail.com>
2020-04-12 14:18:52 +0530

    Encoding/Decoding URL Segments to handle special chars

--- src/main/java/com/viggys/explorer/util/PathUtil.java
+++ src/main/java/com/viggys/explorer/util/PathUtil.java
@@ -3,24 +3,21 @@ package com.viggys.explorer.util;
 import com.viggys.explorer.controller.PathController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.hateoas.Link;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 public class PathUtil {
 
@@ -34,14 +31,16 @@ public class PathUtil {
     }
 
     public static String getInspectUrl(Path path) {
+        String encodedPath = encodeValue(path);
         return linkTo(PathController.class)
-                .slash(path.toString())
+                .slash(encodedPath)
                 .toUri().toString();
     }
 
     public static String getDownloadUrl(Path path) {
+        String encodedPath = encodeValue(path);
         return linkTo(PathController.class)
-                .slash(path.toString().concat(File.separator).concat("download"))
+                .slash(encodedPath.concat(File.separator).concat("download"))
                 .toUri().toString();
     }
 
@@ -50,13 +49,31 @@ public class PathUtil {
             return SystemUtil.getUserHome();
         }
         else {
-            validatePath(path);
-            return Path.of(path);
+            String decodedPath = decodeValue(path);
+            validatePath(decodedPath);
+            return Path.of(decodedPath);
         }
     }
 
     private static void validatePath(String path) {
-        Assert.isTrue(Path.of(path).toFile().exists(),"Path does not exist");
+        Assert.isTrue(Path.of(path).toFile().exists(),"Path [" + path + "] does not exist");
+    }
+
+    private static String encodeValue(Path path) {
+        StringBuilder encodedPathBuilder = new StringBuilder();
+        path.iterator().forEachRemaining(segment -> {
+            String encodedSegment = URLEncoder.encode(segment.toString(), StandardCharsets.UTF_8);
+            encodedPathBuilder.append(File.separator).append(encodedSegment);
+        });
+        String encodedPath = encodedPathBuilder.toString();
+        log.debug("Encoded Path :: [{}]", encodedPath);
+        return encodedPath;
+    }
+
+    private static String decodeValue(String value) {
+        String decodedPath = URLDecoder.decode(value, StandardCharsets.UTF_8);
+        log.debug("Decoded Path :: [{}]", decodedPath);
+        return decodedPath;
     }
 
 }
--- src/test/java/com/viggys/explorer/test/util/PathUtilTest.java
+++ src/test/java/com/viggys/explorer/test/util/PathUtilTest.java
@@ -6,7 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -26,5 +30,19 @@ public class PathUtilTest {
         Path path = SystemUtil.getUserHome();
         String link = PathUtil.getInspectUrl(path);
         System.out.println(link);
+    }
+
+    @Test
+    public void encodeValue() throws MalformedURLException {
+        String value = "/Users/vigneshsubramanian/Documents/My Documents";
+        Path path = PathUtil.resolvePath(value);
+        StringBuilder encodedPathBuilder = new StringBuilder();
+        path.iterator().forEachRemaining(segment -> {
+            String encodedSegment = URLEncoder.encode(segment.toString(), StandardCharsets.UTF_8);
+            encodedPathBuilder.append(File.separator).append(encodedSegment);
+            log.info("[{}]", encodedSegment);
+        });
+        String encodedPath = encodedPathBuilder.toString();
+        log.info("Encoded Path = [{}]", encodedPath);
     }
 }
